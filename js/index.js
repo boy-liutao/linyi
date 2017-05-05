62084b2cfabce4b2465a7782d6d09a05f886a736
boy-liutao <lanou3g@lanou3g-7.local>
2017-05-05 16:51:05 +0800

    临沂鸿光

---
+++ js/index.js
@@ -0,0 +1,144 @@
+;(function(){
+	// 设置高度
+	var fullHeight = function() {
+		$('.js-fullheight').css('height', $(window).height() - 49);
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height() - 49);
+		})
+	};
+	// 向下小标
+	var ScrollNext = function() {
+		$('body').on('click', '.scroll-btn', function(ev){
+			ev.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $( $(this).closest('[data-next="yes"]').next()).offset().top
+			}, 1000, 'easeInOutExpo');
+			return false;
+		});
+	};
+
+	//动画效果添加
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+
+	//flexslider
+	var testimonialFlexslider = function() {
+		var $flexslider = $('.flexslider');
+		$flexslider.flexslider({
+		  animation: "fade",
+		  manualControls: ".flex-control-nav li",
+		  directionNav: false,
+		  smoothHeight: true,
+		  useCSS: false /* Chrome fix*/
+		});
+	}
+
+	// 加载
+	var loaderPage = function() {
+		$(".fh5co-loader").fadeOut("slow");
+	};
+
+	// 点击隐藏部分
+	var mobileMenuOutsideClick = function() {
+
+		$(document).click(function (e) {
+	    var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+
+	    	if ( $('body').hasClass('offcanvas-visible') ) {
+
+    			$('body').removeClass('offcanvas-visible');
+    			$('.js-fh5co-nav-toggle').removeClass('active');
+				
+	    	}
+	    
+	    	
+	    }
+		});
+
+	};
+
+
+	// 隐藏部分
+	var offcanvasMenu = function() {
+		$('body').prepend('<div id="fh5co-offcanvas" />');
+		$('#fh5co-offcanvas').prepend('<ul id="fh5co-side-links">');
+		$('body').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>');
+
+		$('.left-menu li, .right-menu li').each(function(){
+
+			var $this = $(this);
+
+			$('#fh5co-offcanvas ul').append($this.clone());
+
+		});
+	};
+
+	// 隐藏部分
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+			var $this = $(this);
+
+			$('body').toggleClass('fh5co-overflow offcanvas-visible');
+			$this.toggleClass('active');
+			event.preventDefault();
+
+		});
+
+		$(window).resize(function() {
+			if ( $('body').hasClass('offcanvas-visible') ) {
+		   	$('body').removeClass('offcanvas-visible');
+		   	$('.js-fh5co-nav-toggle').removeClass('active');
+		   }
+		});
+
+		$(window).scroll(function(){
+			if ( $('body').hasClass('offcanvas-visible') ) {
+		   	$('body').removeClass('offcanvas-visible');
+		   	$('.js-fh5co-nav-toggle').removeClass('active');
+		   }
+		});
+
+	};
+
+
+
+
+	$(function(){
+		fullHeight();
+		ScrollNext();
+		contentWayPoint();
+		testimonialFlexslider();
+		loaderPage();
+		mobileMenuOutsideClick();
+		offcanvasMenu();
+		burgerMenu();
+	});
+
+
+}());
